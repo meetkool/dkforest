d0f443ce4629bedfdc6b4048e92663513ffddd4f
Meet Bhanushali <iitjeemeet@gmail.com>
2024-03-19 04:05:48 +0530

    improve

--- pkg/database/tableChatReactions.go
+++ pkg/database/tableChatReactions.go
@@ -5,22 +5,29 @@ import (
 )
 
 type ChatReaction struct {
-	ID        int64
-	UserID    UserID
-	MessageID int64
-	Reaction  int64
-	CreatedAt time.Time
+	ID        int64     `gorm:"primaryKey" json:"id"`
+	UserID    UserID    `gorm:"not null" json:"user_id"`
+	MessageID int64     `gorm:"not null" json:"message_id"`
+	Reaction  int64     `gorm:"not null" json:"reaction"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 }
 
 func (d *DkfDB) CreateChatReaction(userID UserID, messageID, reaction int64) error {
-	out := ChatReaction{
+	chatReaction := ChatReaction{
 		UserID:    userID,
 		MessageID: messageID,
 		Reaction:  reaction,
 	}
-	return d.db.Create(&out).Error
+	return d.db.Create(&chatReaction).Error
 }
 
 func (d *DkfDB) DeleteReaction(userID UserID, messageID, reaction int64) error {
-	return d.db.Delete(ChatReaction{}, "user_id = ? AND message_id = ? AND reaction = ?", userID, messageID, reaction).Error
+	result := d.db.Delete(&ChatReaction{}, "user_id = ? AND message_id = ? AND reaction = ?", userID, messageID, reaction)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return ErrNoReactionFound
+	}
+	return nil
 }
