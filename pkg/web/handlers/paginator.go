9260f2e97b37e6e559f2793e62e1dc59b9bf4d30
Meet Bhanushali <iitjeemeet@gmail.com>
2024-03-19 04:05:52 +0530

    improve

--- pkg/web/handlers/paginator.go
+++ pkg/web/handlers/paginator.go
@@ -1,55 +1,42 @@
 package handlers
 
 import (
-	"dkforest/pkg/utils"
+	"fmt"
+	"math"
+	"net/http"
+	"strconv"
+
 	"github.com/labstack/echo"
 	"gorm.io/gorm"
-	"math"
 )
 
 type Paginator struct {
-	resultsPerPage       int64
-	wantedPageQueryParam string
+	ResultsPerPage int64
+	WantedPageQP   string
 }
 
 func NewPaginator() *Paginator {
 	return &Paginator{
-		wantedPageQueryParam: "p",
-		resultsPerPage:       50,
+		ResultsPerPage: 50,
+		WantedPageQP:   "p",
 	}
 }
 
-func (p *Paginator) SetResultPerPage(v int64) *Paginator {
-	p.resultsPerPage = v
+func (p *Paginator) SetResultsPerPage(v int64) *Paginator {
+	p.ResultsPerPage = v
 	return p
 }
 
-func (p *Paginator) SetWantedPageQueryParam(v string) *Paginator {
-	p.wantedPageQueryParam = v
+func (p *Paginator) SetWantedPageQP(v string) *Paginator {
+	p.WantedPageQP = v
 	return p
 }
 
 func (p *Paginator) Paginate(c echo.Context, query *gorm.DB) (int64, int64, int64, *gorm.DB) {
-	wantedPage := utils.DoParseInt64(c.QueryParam(p.wantedPageQueryParam))
-	var count int64
-	query.Session(&gorm.Session{}).Count(&count)
-	resultsPerPage := p.resultsPerPage
-	page, maxPage := paginate(resultsPerPage, wantedPage, count)
-	query = query.Offset(int((page - 1) * resultsPerPage)).Limit(int(resultsPerPage))
-	return page, maxPage, count, query
-}
+	wantedPageStr := c.QueryParam(p.WantedPageQP)
+	wantedPage, err := strconv.ParseInt(wantedPageStr, 10, 64)
+	if err != nil || wantedPage < 1 {
+		wantedPage = 1
+	}
 
-func paginate(resultsPerPage, wantedPage, size int64) (page int64, maxPage int64) {
-	page = wantedPage
-	if page <= 1 {
-		page = 1
-	}
-	maxPage = int64(math.Ceil(float64(size) / float64(resultsPerPage)))
-	if maxPage <= 1 {
-		maxPage = 1
-	}
-	if page > maxPage {
-		page = maxPage
-	}
-	return
-}
+	var count int6
