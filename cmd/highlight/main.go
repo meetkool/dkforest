c8c93c2e0582b89e1fcde53cd35e87a5cec23df6
Meet Bhanushali <iitjeemeet@gmail.com>
2024-03-19 04:05:46 +0530

    improve

--- cmd/highlight/main.go
+++ cmd/highlight/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/alecthomas/chroma/formatters/html"
@@ -14,18 +15,38 @@ func main() {
 	var fileName string
 	flag.StringVar(&fileName, "f", "", "File name")
 	flag.Parse()
-	by, err := os.ReadFile(fileName)
-	if err != nil {
-		panic(err)
+
+	if fileName == "" {
+		fmt.Println("Error: File name is required.")
+		os.Exit(1)
 	}
+
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+		os.Exit(1)
+	}
+
 	lexer := lexers.Match(fileName)
 	style := styles.Get("monokai")
 	formatter := html.New(html.Standalone(true), html.TabWidth(4), html.WithLineNumbers(true), html.LineNumbersInTable(true))
-	iterator, _ := lexer.Tokenise(nil, string(by))
-	buf := bytes.Buffer{}
-	_ = formatter.Format(&buf, style, iterator)
-	out := buf.Bytes()
-	out = bytes.ReplaceAll(out, []byte("{{"), []byte("&#123;&#123;"))
-	out = bytes.ReplaceAll(out, []byte("}}"), []byte("&#125;&#125;"))
-	_ = os.WriteFile("output.html", out, 0644)
-}
+
+	iter, err := lexer.Tokenise(nil, string(data))
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	var buf bytes.Buffer
+	err = formatter.Format(&buf, style, iter)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	htmlBytes := bytes.ReplaceAll(buf.Bytes(), []byte("{{"), []byte("&#123;&#123;"))
+	htmlBytes = bytes.ReplaceAll(htmlBytes, []byte("}}"), []byte("&#125;&#125;"))
+
+	outputFileName := "output.html"
+	err = os.WriteFile(outputFileName, htmlBytes, 0644)
+	if err != nil
