32f0826b824544f3b2868368d6219c9d9379c0bd
Meet Bhanushali <iitjeemeet@gmail.com>
2024-03-19 04:05:50 +0530

    improve

--- pkg/web/handlers/interceptors/interceptor.go
+++ pkg/web/handlers/interceptors/interceptor.go
@@ -1,9 +1,24 @@
 package interceptors
 
 import (
+	"errors"
 	"dkforest/pkg/web/handlers/interceptors/command"
 )
 
 type Interceptor interface {
-	InterceptMsg(*command.Command)
+	Intercept(i interface{}) error
+}
+
+func (i *InterceptorImpl) Intercept(input interface{}) error {
+	switch input.(type) {
+	case *command.Command:
+		i.InterceptMsg(input.(*command.Command))
+	default:
+		return errors.New("unsupported input type")
+	}
+	return nil
+}
+
+type InterceptorImpl struct {
+	InterceptMsg func(*command.Command)
 }
