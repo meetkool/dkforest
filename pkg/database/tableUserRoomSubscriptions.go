2b3d9a9386f448df4ef7cc140640077a4632e148
Meet Bhanushali <iitjeemeet@gmail.com>
2024-03-19 04:05:51 +0530

    improve

--- pkg/database/tableUserRoomSubscriptions.go
+++ pkg/database/tableUserRoomSubscriptions.go
@@ -1,26 +1,27 @@
 package database
 
 import (
-	"time"
-
+	"github.com/jinzhu/gorm"
 	"github.com/sirupsen/logrus"
 )
 
 type UserRoomSubscription struct {
-	UserID    UserID
-	RoomID    RoomID
-	CreatedAt time.Time
-	Room      ChatRoom
+	gorm.Model
+	UserID UserID `gorm:"index;not null"`
+	RoomID RoomID `gorm:"index;not null"`
+	Room   ChatRoom `gorm:"foreignkey:RoomID;association_foreignkey:ID"`
 }
 
-func (s *UserRoomSubscription) DoSave(db *DkfDB) {
+func (s *UserRoomSubscription) Save(db *DkfDB) {
 	if err := db.db.Save(s).Error; err != nil {
 		logrus.Error(err)
 	}
 }
 
 func (d *DkfDB) SubscribeToRoom(userID UserID, roomID RoomID) (err error) {
-	return d.db.Create(&UserRoomSubscription{UserID: userID, RoomID: roomID}).Error
+	subscription := UserRoomSubscription{UserID: userID, RoomID: roomID}
+	err = d.db.Create(&subscription).Error
+	return
 }
 
 func (d *DkfDB) UnsubscribeFromRoom(userID UserID, roomID RoomID) (err error) {
@@ -28,7 +29,7 @@ func (d *DkfDB) UnsubscribeFromRoom(userID UserID, roomID RoomID) (err error) {
 }
 
 func (d *DkfDB) IsUserSubscribedToRoom(userID UserID, roomID RoomID) bool {
-	var count int64
+	var count int
 	d.db.Model(UserRoomSubscription{}).Where("user_id = ? AND room_id = ?", userID, roomID).Count(&count)
 	return count == 1
 }
